f5eb5b04e7f352ed776ec41f31095add2ef2f8bb
Alex Blackburn <alex.blackburn1995@gmail.com>
2022-07-11 14:44:17 -0400

    commit tree e8464d245a52f5839c6efa219179f38a19f23671

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [search, setSearch] =useState("")
+  
+
   useEffect(() => {
     console.log("useEffect called");
   });
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return (
+    <div>
+      <h1>useEffect</h1>
+      <button onClick={() => setCount(count +1)}>Click Me {count}</button>
+      <input type="text" placeholder="Type" value={search} onChange={e => (e.target.value)} />
+
+
+
+
+    </div>
+    
+  )
 }
 
 export default App;
+ 
